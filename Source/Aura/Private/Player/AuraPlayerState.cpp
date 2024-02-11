f7fea1c1c9c7c3ab765c9c25baa75a472de8831d
huzi2 <huzi2@naver.com>
2024-02-11 19:47:04 +0900

    플레이어 상태 클래스 추가. GAS 활성화. 어빌리티 시스템 컴포넌트, 어트리뷰트 세트 클래스 추가

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	// GAS 세트를 플레이어 스테이트에서 사용하려면 업데이트 빈도가 높은 것이 좋다.
+	// 포트나이트나 라이라 같은 경우에도 높게 설정해서 사용한다고함
+	NetUpdateFrequency = 100.f;
+}
