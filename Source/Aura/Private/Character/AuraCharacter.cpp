e01f92da443101ea49a9601d543fa51487b165be
huzi2 <huzi2@naver.com>
2024-11-05 22:55:43 +0900

    입력 대기 누르기 - 마법진 표시할 때 마우스 숨기기 - 비전 파편 사용하면 마법진 표시하고, 버튼 누르면 사라지도록 구현

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -271,6 +271,7 @@ void AAuraCharacter::ShowMagicCircle_Implementation(UMaterialInstance* DecalMate
 	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
 	{
 		AuraPlayerController->ShowMagicCircle(DecalMaterial);
+		AuraPlayerController->bShowMouseCursor = false;
 	}
 }
 
@@ -279,6 +280,7 @@ void AAuraCharacter::HideMagicCircle_Implementation()
 	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
 	{
 		AuraPlayerController->HideMagicCircle();
+		AuraPlayerController->bShowMouseCursor = true;
 	}
 }
 
