50db35a44a4d4a173ce3fa2188e19dae2112aa26
huzi2 <huzi2@naver.com>
2024-11-06 23:32:11 +0900

    포인트 수집 - 포인트 수집 클래스 추가 - 특정 위치의 특정 포인트들을 얻어오는 함수 추가 및 내용 작업 중

---
+++ Source/Aura/Private/Actor/PointCollection.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actor/PointCollection.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+APointCollection::APointCollection()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Pt_0 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_0"));
+	SetRootComponent(Pt_0);
+	ImmutablePts.Add(Pt_0);
+
+	Pt_1 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_1"));
+	Pt_1->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_1);
+
+	Pt_2 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_2"));
+	Pt_2->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_2);
+
+	Pt_3 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_3"));
+	Pt_3->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_3);
+
+	Pt_4 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_4"));
+	Pt_4->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_4);
+
+	Pt_5 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_5"));
+	Pt_5->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_5);
+
+	Pt_6 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_6"));
+	Pt_6->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_6);
+
+	Pt_7 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_7"));
+	Pt_7->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_7);
+
+	Pt_8 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_8"));
+	Pt_8->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_8);
+
+	Pt_9 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_9"));
+	Pt_9->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_9);
+
+	Pt_10 = CreateDefaultSubobject<USceneComponent>(TEXT("Pt_10"));
+	Pt_10->SetupAttachment(GetRootComponent());
+	ImmutablePts.Add(Pt_10);
+}
+
+void APointCollection::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+TArray<USceneComponent*> APointCollection::GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride) const
+{
+	checkf(ImmutablePts.Num() >= NumPoints, TEXT("Attempted to access ImmutablePts out of bounds"));
+
+	TArray<USceneComponent*> ArrayCopy;
+	ArrayCopy.Reserve(NumPoints);
+
+	for (USceneComponent* Pt : ImmutablePts)
+	{
+		if (ArrayCopy.Num() > NumPoints) return ArrayCopy;
+
+		if (Pt != Pt_0)
+		{
+			// 포인트 회전
+			FVector ToPoint = Pt->GetComponentLocation() - Pt_0->GetComponentLocation();
+			ToPoint = ToPoint.RotateAngleAxis(YawOverride, FVector::UpVector);
+			Pt->SetWorldLocation(Pt_0->GetComponentLocation() + ToPoint);
+		}
+
+		const FVector RaisedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z + 500.f);
+		const FVector LoweredLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z - 500.f);
+
+		FHitResult HitResult;
+		TArray<AActor*> IgnoreActors;
+		UAuraAbilitySystemLibrary::GetLivePlayersWithRadius(this, IgnoreActors, TArray<AActor*>(), 1500.f, GetActorLocation());
+	}
+
+	return TArray<USceneComponent*>();
+}
---
+++ Source/Aura/Public/Actor/PointCollection.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PointCollection.generated.h"
+
+/**
+ * 특정 포인트 지점들을 생성하는 액터
+ */
+UCLASS()
+class AURA_API APointCollection : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	APointCollection();
+
+private:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintPure)
+	TArray<USceneComponent*> GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride = 0.f) const;
+
+protected:
+	// 포인트 지점들을 가진 배열
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TArray<USceneComponent*> ImmutablePts;
+
+	// 포인트 지점들
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_0;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_1;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_2;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_3;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_4;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_5;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_6;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_7;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_8;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_9;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_10;
+};
