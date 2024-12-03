67278e55fe33c88b92bde074e935afb8b5f30fdc
huzi2 <huzi2@naver.com>
2024-12-03 22:42:44 +0900

    뷰 모델 구성 - 위젯 블루프린트에 뷰모델 세팅 - 코드에서 뷰 모델 객체를 생성 후 위젯 블루프린트에서 사용할 수 있도록 세팅

--- Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
+++ Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
@@ -1,3 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UI/HUD/LoadScreenHUD.h"
+#include "UI/Widget/LoadScreenWidget.h"
+#include "UI/ViewModel/MVVM_LoadScreen.h"
+
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+
+	if (LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass))
+	{
+		LoadScreenWidget->AddToViewport();
+	}
+}
--- Source/Aura/Public/UI/HUD/LoadScreenHUD.h
+++ Source/Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -6,12 +6,35 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class ULoadScreenWidget;
+class UMVVM_LoadScreen;
+
 /**
- * 
+ * 로드 메뉴에서 사용할 HUD 클래스
  */
 UCLASS()
 class AURA_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+private:
+	virtual void BeginPlay() final;
+
+protected:
+	// 로드 메뉴 UI 객체
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;
+
+	// 로드 메뉴 뷰모델 객체
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;
+
+private:
+	// 로드 메뉴 UI 클래스
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	// 로드 메뉴 뷰모델 클래스
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;
 };
